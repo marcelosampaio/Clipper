ff3595af7abcb51c5fd8bb32fe6ef3976c9c6f42
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2019-07-03 11:14:04 -0300

    getting user's location 💥

--- Clipper/Controller/MainController.swift
+++ Clipper/Controller/MainController.swift
@@ -9,7 +9,12 @@
 import UIKit
 import MapKit
 
-class MainController: UIViewController {
+class MainController: UIViewController, CLLocationManagerDelegate {
+    
+    // MARK: - Propertires
+    private let regionRadius: CLLocationDistance = 1000
+    private let locationManager = CLLocationManager()
+    
     
     // MARK: - Outlets
     
@@ -19,7 +24,46 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // initial procedures
+        initialProcedures()
+
     }
 
+    
+    // MARK: - Map Helpers
+    private func initialProcedures() {
+        
+        // User's permissions for the map
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    
+    private func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
 
+    // MARK: - Location Manager Delegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        
+        // stop updating user's location
+        locationManager.stopUpdatingLocation()
+        
+        print("⭐️⭐️⭐️⭐️⭐️ locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        // init and center map
+        let initialLocation = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
+        centerMapOnLocation(location: initialLocation)
+        
+    }
+    
+    
 }
