591bbbb1fcdb8cb8ea22027e9b79136d7c853e9c
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2019-12-23 12:59:25 -0300

    some new cell types 🥮

---
+++ Clipper/TableView Cell/Locations/LabelCell.swift
@@ -0,0 +1,26 @@
+//
+//  LabelCell.swift
+//  Clipper
+//
+//  Created by Marcelo Sampaio on 23/12/19.
+//  Copyright © 2019 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class LabelCell: UITableViewCell {
+    
+    // MARK: - Outlets
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Clipper/TableView Cell/Locations/TextCell.swift
@@ -0,0 +1,27 @@
+//
+//  TextCell.swift
+//  Clipper
+//
+//  Created by Marcelo Sampaio on 23/12/19.
+//  Copyright © 2019 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class TextCell: UITableViewCell {
+
+    // MARK: - Outlets
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
